46e1e1c5a5f8b365a15e9afeed82fe6d19484f05
Peter Bjorklund <piot@hotmail.com>
2024-01-29 21:00:55 +0100

    feat: add Out- and InOctetStream

--- src/lib.rs
+++ src/lib.rs
@@ -1,102 +1,136 @@
+use std::io::{Cursor, Read, Result};
+
 pub trait WriteOctetStream {
-    fn write_u32(&mut self, v: u32) -> std::io::Result<()>;
-    fn write_i32(&mut self, v: i32) -> std::io::Result<()>;
-    fn write_u16(&mut self, v: u16) -> std::io::Result<()>;
-    fn write_i16(&mut self, v: i16) -> std::io::Result<()>;
-    fn write_u8(&mut self, v: u8) -> std::io::Result<()>;
-    fn write_i8(&mut self, v: i8) -> std::io::Result<()>;
+    fn write_u32(&mut self, v: u32) -> Result<()>;
+    fn write_i32(&mut self, v: i32) -> Result<()>;
+    fn write_u16(&mut self, v: u16) -> Result<()>;
+    fn write_i16(&mut self, v: i16) -> Result<()>;
+    fn write_u8(&mut self, v: u8) -> Result<()>;
+    fn write_i8(&mut self, v: i8) -> Result<()>;
 }
 
 pub trait ReadOctetStream {
-    fn read_u32(&mut self) -> std::io::Result<u32>;
-    fn read_i32(&mut self) -> std::io::Result<i32>;
-    fn read_u16(&mut self) -> std::io::Result<u16>;
-    fn read_i16(&mut self) -> std::io::Result<i16>;
-    fn read_u8(&mut self) -> std::io::Result<u8>;
-    fn read_i8(&mut self) -> std::io::Result<i8>;
+    fn read_u32(&mut self) -> Result<u32>;
+    fn read_i32(&mut self) -> Result<i32>;
+    fn read_u16(&mut self) -> Result<u16>;
+    fn read_i16(&mut self) -> Result<i16>;
+    fn read_u8(&mut self) -> Result<u8>;
+    fn read_i8(&mut self) -> Result<i8>;
+}
+
+pub struct OutOctetStream {
+    pub data: Vec<u8>,
+}
+
+impl OutOctetStream {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+}
+
+impl Default for OutOctetStream {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl WriteOctetStream for OutOctetStream {
+    fn write_u32(&mut self, v: u32) -> Result<()> {
+        self.data.push((v >> 24) as u8);
+        self.data.push((v >> 16) as u8);
+        self.data.push((v >> 8) as u8);
+        self.data.push(v as u8);
+        Ok(())
+    }
+
+    fn write_i32(&mut self, v: i32) -> Result<()> {
+        self.write_u32(v as u32)
+    }
+
+    fn write_u16(&mut self, v: u16) -> Result<()> {
+        self.data.push((v >> 8) as u8);
+        self.data.push(v as u8);
+        Ok(())
+    }
+
+    fn write_i16(&mut self, v: i16) -> Result<()> {
+        self.write_u16(v as u16)
+    }
+
+    fn write_u8(&mut self, v: u8) -> Result<()> {
+        self.data.push(v);
+        Ok(())
+    }
+
+    fn write_i8(&mut self, v: i8) -> Result<()> {
+        self.write_u8(v as u8)
+    }
+}
+
+pub struct InOctetStream {
+    pub cursor: Cursor<Vec<u8>>,
+}
+
+impl InOctetStream {
+    pub fn new(data: Vec<u8>) -> Self {
+        Self {
+            cursor: Cursor::new(data.clone()),
+        }
+    }
+}
+
+impl ReadOctetStream for InOctetStream {
+    fn read_u32(&mut self) -> Result<u32> {
+        let mut buf = [0; 4];
+        self.cursor.read_exact(&mut buf)?;
+        Ok(u32::from_be_bytes(buf))
+    }
+    fn read_i32(&mut self) -> Result<i32> {
+        let mut buf = [0; 4];
+        self.cursor.read_exact(&mut buf)?;
+        Ok(i32::from_be_bytes(buf))
+    }
+
+    fn read_u16(&mut self) -> Result<u16> {
+        let mut buf = [0; 2];
+        self.cursor.read_exact(&mut buf)?;
+        Ok(u16::from_be_bytes(buf))
+    }
+
+    fn read_i16(&mut self) -> Result<i16> {
+        let mut buf = [0; 2];
+        self.cursor.read_exact(&mut buf)?;
+        Ok(i16::from_be_bytes(buf))
+    }
+
+    fn read_u8(&mut self) -> Result<u8> {
+        let mut buf = [0; 1];
+        self.cursor.read_exact(&mut buf)?;
+        Ok(buf[0])
+    }
+
+    fn read_i8(&mut self) -> Result<i8> {
+        let mut buf = [0; 1];
+        self.cursor.read_exact(&mut buf)?;
+        Ok(buf[0] as i8)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io::{Error, ErrorKind, Result};
 
     use super::*;
 
-    pub struct TestWriteOctetStream {
-        pub data: Vec<u8>,
-    }
-
-    impl TestWriteOctetStream {
-        pub fn new() -> Self {
-            Self { data: Vec::new() }
-        }
-    }
-
-    pub struct TestReadOctetStream {
-        pub data: Vec<u8>,
-    }
-
-    impl WriteOctetStream for TestWriteOctetStream {
-        fn write_u32(&mut self, _v: u32) -> Result<()> {
-            //self.data.push(v);
-            Ok(())
-        }
-
-        fn write_i32(&mut self, v: i32) -> Result<()> {
-            self.write_u32(v as u32)
-        }
-
-        fn write_u16(&mut self, _v: u16) -> Result<()> {
-            //self.data.push(v);
-            Ok(())
-        }
-
-        fn write_i16(&mut self, v: i16) -> Result<()> {
-            self.write_u16(v as u16)
-        }
-
-        fn write_u8(&mut self, v: u8) -> Result<()> {
-            self.data.push(v);
-            Ok(())
-        }
-
-        fn write_i8(&mut self, v: i8) -> Result<()> {
-            self.write_u8(v as u8)
-        }
-    }
-
-    impl ReadOctetStream for TestReadOctetStream {
-        fn read_i32(&mut self) -> Result<i32> {
-            Ok(0)
-        }
-
-        fn read_u32(&mut self) -> Result<u32> {
-            Ok(0)
-        }
-        fn read_i16(&mut self) -> Result<i16> {
-            Ok(0)
-        }
-
-        fn read_u16(&mut self) -> Result<u16> {
-            //self.data.pop()
-            Ok(0)
-        }
-
-        fn read_i8(&mut self) -> Result<i8> {
-            Ok(0)
-        }
-
-        fn read_u8(&mut self) -> Result<u8> {
-            self.data
-                .pop()
-                .ok_or_else(|| Error::new(ErrorKind::Other, "No more data"))
-        }
-    }
-
     #[test]
     fn write_u32() {
-        let mut out_stream = TestWriteOctetStream::new();
-        let result = out_stream.write_u32(0x12345678);
+        const EXPECTED_U32: u32 = 0x12345678;
+        let mut out_stream = OutOctetStream::new();
+        let result = out_stream.write_u32(EXPECTED_U32);
         assert!(result.is_ok());
+
+        let mut in_stream = InOctetStream::new(out_stream.data);
+        let result = in_stream.read_u32();
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), EXPECTED_U32);
     }
 }
