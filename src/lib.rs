7f55544885776ea21332d1f7ad445a105f23f09e
Peter Bjorklund <piot@hotmail.com>
2024-08-22 14:50:50 +0200

    feat: add `has_reached_end`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+/*
+ * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/flood-rs
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
 use std::io::{Cursor, Read, Result};
+
+mod test;
 
 pub trait WriteOctetStream {
     fn write(&mut self, v: &[u8]) -> Result<()>;
@@ -24,6 +30,7 @@ pub trait ReadOctetStream {
     fn read_u8(&mut self) -> Result<u8>;
     fn read_i8(&mut self) -> Result<i8>;
     fn verify_debug_marker(&mut self, expected: u8) -> Result<()>;
+    fn has_reached_end(&self) -> bool;
 }
 
 pub struct OutOctetStream {
@@ -196,58 +203,8 @@ impl ReadOctetStream for InOctetStream {
         }
         Ok(())
     }
-}
 
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn write_u32() {
-        const EXPECTED_U32: u32 = 0x12345678;
-        let mut out_stream = OutOctetStream::new();
-        let result = out_stream.write_u32(EXPECTED_U32);
-        assert!(result.is_ok());
-
-        let mut in_stream = InOctetStream::new(out_stream.data);
-        let result = in_stream.read_u32();
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), EXPECTED_U32);
-    }
-
-    #[test]
-    fn write_markers() {
-        const EXPECTED_U32: u32 = 0x12345678;
-        let mut out_stream = OutOctetStream::new();
-        out_stream.should_write_markers = true;
-        out_stream.write_debug_marker(0xcb).unwrap();
-        let result = out_stream.write_u32(EXPECTED_U32);
-        assert_eq!(out_stream.data.len(), 5);
-
-        assert!(result.is_ok());
-
-        let mut in_stream = InOctetStream::new(out_stream.data);
-        in_stream.should_verify_markers = true;
-        in_stream.verify_debug_marker(0xcb).unwrap();
-        let result = in_stream.read_u32();
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), EXPECTED_U32);
-    }
-
-    #[test]
-    fn skip_write_markers() {
-        const EXPECTED_U32: u32 = 0x12345678;
-        let mut out_stream = OutOctetStream::new();
-        out_stream.write_debug_marker(0xcb).unwrap();
-        let result = out_stream.write_u32(EXPECTED_U32);
-        assert_eq!(out_stream.data.len(), 4);
-        assert!(result.is_ok());
-
-        let mut in_stream = InOctetStream::new(out_stream.data);
-        in_stream.verify_debug_marker(0xcb).unwrap();
-        let result = in_stream.read_u32();
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), EXPECTED_U32);
+    fn has_reached_end(&self) -> bool {
+        self.cursor.position() as usize == self.cursor.get_ref().len()
     }
 }
